add9219bfc242e7178be0de11c9972f17d07022b
Riccardo Maria Frighi <r.frighi@campus.unimib.it>
2019-10-29 17:06:38 +0100

    Added dependency and modified App.java

--- quickstart/src/main/java/riccardo/quickstart/App.java
+++ quickstart/src/main/java/riccardo/quickstart/App.java
@@ -1,13 +1,11 @@
 package riccardo.quickstart;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import com.google.gson.Gson;
+
+public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson( "Hello World!" ) );
     }
 }
